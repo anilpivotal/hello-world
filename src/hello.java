cb181a37eee40a1e56a28f23f36d38c85a614bad
anilpivotal <50462530+anilpivotal@users.noreply.github.com>
2019-05-17 08:44:02 -0400

    Update hello.java

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,7 @@
 
 public class hello {
 	public static void main(String[] args) {
-        System.out.println("Hello New World5!"); // Display the string.
+        System.out.println("Hello New World6!"); // Display the string.
     }
 
 }
