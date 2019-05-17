189151c5e6a7829e081eb58902c7bfbe62bc7c5f
anilpivotal <anair@pivotal.io>
2019-05-17 08:49:32 -0400

    new change

--- src/hello.java
+++ src/hello.java
@@ -1,11 +1,9 @@
 
 public class hello {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello New World-7!"); // Display the string.
-=======
-        System.out.println("Hello New World6!"); // Display the string.
->>>>>>> branch 'master' of https://github.com/anilpivotal/hello-world.git
+
+        System.out.println("Hello New World-8!"); // Display the string.
+
     }
 
 }
